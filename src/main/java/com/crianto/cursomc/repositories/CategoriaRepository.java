7ad449acdae3848b536b2edc640e160595ff78bf
arcnrick <arcnrick@gmail.com>
2018-06-05 22:09:46 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/crianto/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.crianto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crianto.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
---
+++ src/main/java/com/crianto/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.crianto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crianto.cursomc.domain.Categoria;
+import com.crianto.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	/* declarar dependencia com notação Autowired... isso faz com que através da injeção de dependência
+	ou inversão de controle, tal dependência é automaticamente instanciada pelo Spring */ 
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);		
+	}
+}
