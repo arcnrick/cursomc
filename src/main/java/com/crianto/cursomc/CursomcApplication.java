b9cdff53b214185b618bb703a0c35ce924a5cdf6
arcnrick <arcnrick@gmail.com>
2018-06-05 22:42:18 -0300

    Criando operação de instanciação

--- src/main/java/com/crianto/cursomc/CursomcApplication.java
+++ src/main/java/com/crianto/cursomc/CursomcApplication.java
@@ -1,12 +1,43 @@
 package com.crianto.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.crianto.cursomc.domain.Categoria;
+import com.crianto.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		/*
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		categoriaRepository.saveAll(lista);
+		
+		tbm pode fazer direto.. veja abaixo
+		*/
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
 	}
 }
