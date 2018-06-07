d890c4a4235b66148820a39674f549120545f7e1
arcnrick <arcnrick@gmail.com>
2018-06-07 00:21:42 -0300

    Estado e cidade

---
+++ src/main/java/com/crianto/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.crianto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crianto.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
