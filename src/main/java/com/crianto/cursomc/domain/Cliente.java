65d19f34382b6a77b1c42c879210ffc9c58a012a
arcnrick <arcnrick@gmail.com>
2018-06-18 23:32:37 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/crianto/cursomc/domain/Cliente.java
+++ src/main/java/com/crianto/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.crianto.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable {
@@ -28,6 +29,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo; // internamente vai ser Integer, mas vai ser exposto como Tipocliente
 	
+	@JsonManagedReference	// isso aki é para listar junto, os endereços associados
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
---
+++ src/main/java/com/crianto/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.crianto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crianto.cursomc.domain.Cliente;
+import com.crianto.cursomc.repositories.ClienteRepository;
+import com.crianto.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	/* declarar dependencia com notação Autowired... isso faz com que através da injeção de dependência
+	ou inversão de controle, tal dependência é automaticamente instanciada pelo Spring */ 
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado. Id: " + id + ", Tipo: " + Cliente.class.getName()));		
+	}
+	
+}
