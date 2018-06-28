7ebd1d6ce2e8d493d8e1fe7ed7ea7a4f6e73aed9
arcnrick <arcnrick@gmail.com>
2018-06-28 13:39:31 -0300

    Listando todas categorias com DTO

--- src/main/java/com/crianto/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/crianto/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -16,16 +16,14 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		
-		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);		
 	}
 	
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
-		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);		
 	}
 	
--- src/main/java/com/crianto/cursomc/services/CategoriaService.java
+++ src/main/java/com/crianto/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.crianto.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +43,10 @@ public class CategoriaService {
 			repo.deleteById(id);			
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir categoria que possui produtos");
-		}
-		
+		}		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
