499d2ea76fa6210aebeb4c441f8401bc8cd96e08
arcnrick <arcnrick@gmail.com>
2018-07-03 22:10:48 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/crianto/cursomc/services/CategoriaService.java
+++ src/main/java/com/crianto/cursomc/services/CategoriaService.java
@@ -63,4 +63,5 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
+	
 }
--- src/main/java/com/crianto/cursomc/services/ClienteService.java
+++ src/main/java/com/crianto/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.crianto.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.crianto.cursomc.domain.Cliente;
+import com.crianto.cursomc.domain.Cliente;
+import com.crianto.cursomc.dto.ClienteDTO;
 import com.crianto.cursomc.repositories.ClienteRepository;
+import com.crianto.cursomc.services.exceptions.DataIntegrityException;
 import com.crianto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,5 +30,44 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado. Id: " + id + ", Tipo: " + Cliente.class.getName()));		
 	}
+
+	public Cliente update(Cliente obj) {
+		// verificar se o id existe, antes de fazer o update
+		Cliente newObj = find(obj.getId());
+		
+		// a função abaixo serve para buscar os dados do banco e atualizar somente aqueles passados no parâmetro obj
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
+	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir cliente que que já tem pedido");
+		}		
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);		
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);		
+		//throw new UnsupportedOperationException();
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());	
+	}
+
 }
