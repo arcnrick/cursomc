4c6fe296fbd21167a49ac8a281b07ffc9658e8b1
arcnrick <arcnrick@gmail.com>
2018-07-09 22:40:47 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/crianto/cursomc/domain/Produto.java
+++ src/main/java/com/crianto/cursomc/domain/Produto.java
@@ -32,8 +32,7 @@ public class Produto implements Serializable {
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 			joinColumns = @JoinColumn(name = "produto_id"),
-			inverseJoinColumns = @JoinColumn(name = "categoria_id")
-	)
+			inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	@JsonIgnore
